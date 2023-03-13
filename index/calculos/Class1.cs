ce171bb80ab9a8d632e3576b0d3b606c99f23652
Guilherme Andreta <guilherme.andretasantos@hotmail.com>
2023-03-13 11:37:17 -0300

    First Commit

---
+++ index/calculos/Class1.cs
@@ -0,0 +1,46 @@
+﻿namespace calculos {
+    public class AreasGeometricas {
+        public double valor1, valor2, valor3;
+
+        public double Triangulo(double v1, double v2, double v3) {
+            double area;
+
+            area = v1 * v3 / 2;
+
+            return area;
+        }
+
+        public double Circulo(double v1, double v2, double v3) {
+            double area;
+            double pi = 3.14159;
+
+            area = pi * v3 * v3;
+
+            return area;
+        }
+
+        public double Trapezio(double v1, double v2, double v3) {
+            double area;
+
+            area = (v1 + v2) * v3 / 2;
+
+            return area;
+        }
+
+        public double Quadrado(double v1, double v2, double v3) {
+            double area;
+
+            area = v2 * v2;
+
+            return area;
+        }
+
+        public double Retangulo(double v1, double v2, double v3) {
+            double area;
+
+            area = v1 * v2;
+
+            return area;
+        }
+    }
+}
---
+++ index/index/Program.cs
@@ -0,0 +1,34 @@
+﻿using calculos;
+
+internal class Program {
+    private static void Main(string[] args) {
+        Console.WriteLine("Vamos calcular areas geometricas: ");
+        Console.WriteLine("Digite o primeiro numero: ");
+        string n1 = Console.ReadLine();
+
+        Console.WriteLine("Digite o segundo numero: ");
+        string n2 = Console.ReadLine();
+
+        Console.WriteLine("Digite o terceiro numero: ");
+        string n3 = Console.ReadLine();
+
+        AreasGeometricas ag = new AreasGeometricas();
+
+        ag.valor1 = double.Parse(n1);
+        ag.valor2 = double.Parse(n2);
+        ag.valor3 = double.Parse(n3);
+
+        double triangulo = ag.Triangulo(ag.valor1, ag.valor2, ag.valor3);
+        double circulo = ag.Circulo(ag.valor1, ag.valor2, ag.valor3);
+        double trapezio = ag.Trapezio(ag.valor1, ag.valor2, ag.valor3);
+        double quadrado = ag.Quadrado(ag.valor1, ag.valor2, ag.valor3);
+        double retangulo = ag.Retangulo(ag.valor1, ag.valor2, ag.valor3);
+
+        Console.WriteLine("As Areas são: ");
+        Console.WriteLine("Triangulo: " + triangulo.ToString("F3"));
+        Console.WriteLine("Circulo: " + circulo.ToString("F3"));
+        Console.WriteLine("Trapezio: " + trapezio.ToString("F3"));
+        Console.WriteLine("Quadrado: " + quadrado.ToString("F3"));
+        Console.WriteLine("Reatangulo: " + retangulo.ToString("F3"));
+    }
+}
